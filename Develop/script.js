51b22d1fd2962b4cacb4a6680f83d91390c9284c
Code <sangw009@umn.edu>
2022-10-03 17:15:39 -0500

    created feature to save data to local storage

--- Develop/script.js
+++ Develop/script.js
@@ -2,5 +2,14 @@
 var todayDate = moment().format("MMM Do YY");
 $('#currentDay').text(todayDate);
 
+// showing current hour
 var todayHour = moment().format('h:mm:ss a');
-console.log(todayHour)
+console.log(todayHour);
+
+// save button
+var save = document.getElementById("saveBtn");
+function saveData(){
+    var inputData = document.getElementById("message");
+    localStorage.setItem('Reminder', inputData.value);
+}
+save.addEventListener("click", saveData);
